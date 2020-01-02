617098415f3941d4b7ea2ebefec0be12d477c05b
jack-scott <jack.scott6@hotmail.co.uk>
2020-01-03 01:06:06 +1100

    added code to control 5 Neopixels which will light when a device is detected and then fade until unless another ping is detected

--- FriendDetector/FriendDetector.ino
+++ FriendDetector/FriendDetector.ino
@@ -1,24 +1,40 @@
 #include "./esppl_functions.h"
+#include <Adafruit_NeoPixel.h>
+
 
 /*
  * Define you friend's list size here
  */
-#define LIST_SIZE 2
+#define NUMPIXELS 5
+#define PIXEL_PIN 4
+
+#define LIST_SIZE 5
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIXEL_PIN, NEO_GRB + NEO_KHZ400);
+
 /*
  * This is your friend's MAC address list
  */
 uint8_t friendmac[LIST_SIZE][ESPPL_MAC_LEN] = {
-   {0x11, 0x11, 0x11, 0x11, 0x11, 0x11}
-  ,{0x22, 0x22, 0x22, 0x22, 0x22, 0x22}
+  {0xAC, 0x5F, 0x3E, 0x80, 0xC5, 0xAF}
+  ,{0x60, 0x30, 0xD4, 0x02, 0x6E, 0xD9}
+  ,{0x7C, 0x9A, 0x1D, 0x76, 0x80, 0x4C}	
+  ,{0x94, 0xBF, 0x2D, 0x43, 0x30, 0x6A}
+  ,{0xC0, 0xE8, 0x62, 0x25, 0x4E, 0x7F}
   };
 /*
  * This is your friend's name list
  * put them in the same order as the MAC addresses
  */
 String friendname[LIST_SIZE] = {
-   "Friend 1"
-  ,"Friend 2"
+  "Jack"
+  ,"Ollie"
+  ,"Tim"
+  ,"Sam"
+  ,"Ed"
   };
+
+int lastseen[LIST_SIZE];
 
 bool maccmp(uint8_t *mac1, uint8_t *mac2) {
   for (int i=0; i < ESPPL_MAC_LEN; i++) {
@@ -32,14 +48,30 @@ bool maccmp(uint8_t *mac1, uint8_t *mac2) {
 void cb(esppl_frame_info *info) {
   for (int i=0; i<LIST_SIZE; i++) {
     if (maccmp(info->sourceaddr, friendmac[i]) || maccmp(info->receiveraddr, friendmac[i])) {
-      Serial.printf("\n%s is here! :)", friendname[i].c_str());
+        if(lastseen[i] < 255){
+          lastseen[i] = lastseen[i] + 32;
+          if(lastseen[i] > 255){
+            lastseen[i] = 255;
+          }
+        }
+        Serial.printf("\n%s is home : %d", friendname[i].c_str(), lastseen[i]);
+    }else if(lastseen[i] >= 0){
+      if(lastseen[i] > 0){
+        lastseen[i] = lastseen[i] - 1;
+      }
+      Serial.printf("\n%s is NOT home : %d", friendname[i].c_str(), lastseen[i]);
     }
+
+    pixels.setPixelColor(i, pixels.Color(0,lastseen[i],0));
+    pixels.show();
+
   }
 }
 
 void setup() {
   delay(500);
   Serial.begin(115200);
+  pixels.begin();
   esppl_init(cb);
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Friend Detector
-## For more information goto: https://www.hackster.io/ricardooliveira/esp8266-friend-detector-12542e
+This is a fork of Ricardo Oliveriera's code (taken on 3/1/20) located at: https://github.com/RicardoOliveira/FriendDetector
+
+For more information goto: https://www.hackster.io/ricardooliveira/esp8266-friend-detector-12542e
+
+##To Flash
+I am using the ESP8266 NodeMCU LoLin breakout board. On this board digital pin 4 is actually connected to D2 on the breakout.
+The following settings were used in the Arduino IDE to upload the code:
+Board: NodeMCU 1.0(ESP8266 12e Module)
+CPU freq: 80Mhz
+Flash: 4M(3M SPIFFS)
+Upload Speed: 115200
+Programmer: AVRISP mkII
